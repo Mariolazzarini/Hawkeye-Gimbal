2da5cf5d81fb84dc7186d277982cbb39accdf3df
Tus muertos Eka!!!! <mariolazzarinisola@gmail.com>
2026-09-18 03:59:31 +0200

    Include custom_tracker.yaml in setup.py
    Add custom_tracker.yaml to package data files

--- src/sspid2/setup.py
+++ src/sspid2/setup.py
@@ -10,6 +10,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', [
+            'sspid2/config/custom_tracker.yaml',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
